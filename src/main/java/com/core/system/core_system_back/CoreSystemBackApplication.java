3251a4c47b4b5d0fd1227d7172bba901f19349a2
diogoandrade2 <diogo.cesar.cel@gmail.com>
2024-11-06 13:33:34 -0300

    init project

---
+++ src/main/java/com/core/system/core_system_back/CoreSystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.core.system.core_system_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSystemBackApplication.class, args);
+	}
+
+}
